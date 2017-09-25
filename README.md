e2550f76bb0cfeb1460768f5ead012d490177a5a
Joel Dare <joel@joeldare.com>
2017-09-25 07:37:38 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bubbles
+
+This app creates bubble paper, a type of graph paper with randomly sized circles. Right click on the image to save it and then print it from your favorite graphics app. I created it for a simple art project.
+
+## Setup
+
+Bubbles is written in HTML and JavaScript. Simply download or clone this repo, make sure `bubbles.js` and `index.html` are in the same directory, then open `index.html` with your web browser of choice.
---
+++ bubbles.js
@@ -0,0 +1,44 @@
+class Bubbles {
+	randomNumber(min,max) {
+	    return Math.floor(Math.random()*(max-min+1)+min);
+	}
+	// Default 80 tall, 60 wide (87 per inch)
+	draw(canvas, width=60, height=80, grid=50) {
+		// Grab the canvas element that was passed
+		var canvasContext = canvas.getContext("2d");
+		var halfGrid = grid/2;
+		var x = 0;
+		var y = 0;
+
+		// Set the canvas size
+		console.log('Size: ' + width + 'x' + height);
+		canvas.width = width*grid;
+		canvas.height = height*grid;
+
+		// Loop through columns
+		for(x=0; x<=width; x++) {
+			// Loop through rows
+			for(y=0; y<=height; y++) {
+				// Calculate the center of this grid square
+				var bubbleX = x * grid + halfGrid;
+				var bubbleY = y * grid + halfGrid;
+
+				// TODO: Pick a random bubble size offset +/- 25%
+				var maxOffsetSize = grid * 0.25;
+				console.log('Max: ' + maxOffsetSize);
+				var bubbleSizeOffset = this.randomNumber(2, maxOffsetSize);
+				var bubbleDiameter = (grid/2) - bubbleSizeOffset;
+				console.log('Size: ' + bubbleDiameter + ' Offset:' + bubbleSizeOffset);
+
+				// Draw this circle
+				canvasContext.beginPath();
+				canvasContext.arc(bubbleX, bubbleY, bubbleDiameter, 0, 2 * Math.PI);
+				canvasContext.stroke();
+			}
+		}
+	}
+}
+
+var drawCanvas = document.getElementById('bubbles');
+var bubbles = new Bubbles;
+bubbles.draw(drawCanvas);
