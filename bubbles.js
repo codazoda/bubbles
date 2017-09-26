18b2bb2814cdb9d17d4a3d8e946c98b88e3ce7e7
Joel Dare <joel@joeldare.com>
2017-09-26 13:37:49 -0600

    increase stroke width

--- bubbles.js
+++ bubbles.js
@@ -2,7 +2,7 @@ class Bubbles {
 	randomNumber(min,max) {
 	    return Math.floor(Math.random()*(max-min+1)+min);
 	}
-	// Default 80 tall, 60 wide (87 per inch)
+	// Default 80 tall, 60 wide (8 per inch)
 	draw(canvas, width=60, height=80, grid=50) {
 		// Grab the canvas element that was passed
 		var canvasContext = canvas.getContext("2d");
@@ -14,6 +14,7 @@ class Bubbles {
 		console.log('Size: ' + width + 'x' + height);
 		canvas.width = width*grid;
 		canvas.height = height*grid;
+		canvasContext.lineWidth = 3;
 
 		// Loop through columns
 		for(x=0; x<=width; x++) {
@@ -26,7 +27,7 @@ class Bubbles {
 				// TODO: Pick a random bubble size offset +/- 25%
 				var maxOffsetSize = grid * 0.25;
 				console.log('Max: ' + maxOffsetSize);
-				var bubbleSizeOffset = this.randomNumber(2, maxOffsetSize);
+				var bubbleSizeOffset = this.randomNumber(4, maxOffsetSize);
 				var bubbleDiameter = (grid/2) - bubbleSizeOffset;
 				console.log('Size: ' + bubbleDiameter + ' Offset:' + bubbleSizeOffset);
 
